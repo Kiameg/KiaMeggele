63274bb2731a895b09f069ab54e8689f3698fdfb
Kiameg <kiax1262@stud.kea.dk>
2025-03-09 16:07:25 +0100

    bg

--- src/app/cv/page.js
+++ src/app/cv/page.js
@@ -4,7 +4,7 @@ export default function CV() {
   return (
     <>
 
-      <h1 className="text-center mb-20 mt-20 text-3xl">All projects</h1>
+      <h1 className="text-center mb-20 mt-20 text-3xl">CV</h1>
 
     </>
   );
