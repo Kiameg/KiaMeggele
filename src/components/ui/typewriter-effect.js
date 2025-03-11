c12eb242abf6a53849e2046fa0e904961b5b8bef
Martin Bruun <martinbruun1995@gmail.com>
2025-03-12 00:42:44 +0100

    Update Tailwind config and typewriter effect component with styling improvements

--- src/components/ui/typewriter-effect.js
+++ src/components/ui/typewriter-effect.js
@@ -1,19 +1,15 @@
-"use client";
+'use client';
 
-import { cn } from "@/lib/utils";
-import { motion, stagger, useAnimate, useInView } from "motion/react";
-import { useEffect } from "react";
+import { cn } from '@/lib/utils';
+import { motion, stagger, useAnimate, useInView } from 'motion/react';
+import { useEffect } from 'react';
 
-export const TypewriterEffect = ({
-  words,
-  className,
-  cursorClassName
-}) => {
+export const TypewriterEffect = ({ words, className, cursorClassName }) => {
   // split text inside of words into array of characters
   const wordsArray = words.map((word) => {
     return {
       ...word,
-      text: word.text.split(""),
+      text: word.text.split(''),
     };
   });
 
@@ -21,44 +17,50 @@ export const TypewriterEffect = ({
   const isInView = useInView(scope);
   useEffect(() => {
     if (isInView) {
-      animate("span", {
-        display: "inline-block",
-        opacity: 1,
-        width: "fit-content",
-      }, {
-        duration: 0.3,
-        delay: stagger(0.1),
-        ease: "easeInOut",
-      });
+      animate(
+        'span',
+        {
+          display: 'inline-block',
+          opacity: 1,
+          width: 'fit-content',
+        },
+        {
+          duration: 0.3,
+          delay: stagger(0.1),
+          ease: 'easeInOut',
+        }
+      );
     }
   }, [isInView]);
 
   const renderWords = () => {
     return (
-      (<motion.div ref={scope} className="inline">
+      <motion.div ref={scope} className="inline">
         {wordsArray.map((word, idx) => {
           return (
-            (<div key={`word-${idx}`} className="inline-block">
+            <div key={`word-${idx}`} className="inline-block pr-2 font-playfair">
               {word.text.map((char, index) => (
                 <motion.span
                   initial={{}}
                   key={`char-${index}`}
-                  className={cn(`dark:text-white text-black opacity-0 hidden`, word.className)}>
+                  className={cn(`dark:text-white text-black opacity-0 hidden`, word.className)}
+                >
                   {char}
                 </motion.span>
               ))}
-            </div>)
+            </div>
           );
         })}
-      </motion.div>)
+      </motion.div>
     );
   };
   return (
-    (<div
+    <div
       className={cn(
-        "text-base sm:text-xl md:text-3xl lg:text-5xl font-bold text-center",
+        'text-base sm:text-xl md:text-3xl lg:text-5xl font-bold text-center',
         className
-      )}>
+      )}
+    >
       {renderWords()}
       <motion.span
         initial={{
@@ -70,70 +72,67 @@ export const TypewriterEffect = ({
         transition={{
           duration: 0.8,
           repeat: Infinity,
-          repeatType: "reverse",
+          repeatType: 'reverse',
         }}
-        className={cn(
-          "inline-block rounded-sm w-[4px] h-4 md:h-6 lg:h-10",
-          cursorClassName
-        )}></motion.span>
-    </div>)
+        className={cn('inline-block rounded-sm w-[4px] h-4 md:h-6 lg:h-10', cursorClassName)}
+      ></motion.span>
+    </div>
   );
 };
 
-export const TypewriterEffectSmooth = ({
-  words,
-  className,
-  cursorClassName
-}) => {
+export const TypewriterEffectSmooth = ({ words, className, cursorClassName }) => {
   // split text inside of words into array of characters
   const wordsArray = words.map((word) => {
     return {
       ...word,
-      text: word.text.split(""),
+      text: word.text.split(''),
     };
   });
   const renderWords = () => {
     return (
-      (<div>
+      <div>
         {wordsArray.map((word, idx) => {
           return (
-            (<div key={`word-${idx}`} className="inline-block">
+            <div key={`word-${idx}`} className="inline-block">
               {word.text.map((char, index) => (
                 <span
                   key={`char-${index}`}
-                  className={cn(`dark:text-white text-black `, word.className)}>
+                  className={cn(`dark:text-white text-black `, word.className)}
+                >
                   {char}
                 </span>
               ))}
-            </div>)
+            </div>
           );
         })}
-      </div>)
+      </div>
     );
   };
 
   return (
-    (<div className={cn("flex space-x-1 my-6", className)}>
+    <div className={cn('flex space-x-1 my-6', className)}>
       <motion.div
         className="overflow-hidden pb-2"
         initial={{
-          width: "0%",
+          width: '0%',
         }}
         whileInView={{
-          width: "fit-content",
+          width: 'fit-content',
         }}
         transition={{
           duration: 2,
-          ease: "linear",
+          ease: 'linear',
           delay: 1,
-        }}>
+        }}
+      >
         <div
           className="text-xs sm:text-base md:text-xl lg:text:3xl xl:text-5xl font-bold"
           style={{
-            whiteSpace: "nowrap",
-          }}>
-          {renderWords()}{" "}
-        </div>{" "}
+            whiteSpace: 'nowrap',
+          }}
+        >
+          {renderWords()}{' '}
+        </div>{' '}
       </motion.div>
       <motion.span
         initial={{
@@ -146,12 +145,10 @@ export const TypewriterEffectSmooth = ({
           duration: 0.8,
 
           repeat: Infinity,
-          repeatType: "reverse",
+          repeatType: 'reverse',
         }}
-        className={cn(
-          "block rounded-sm w-[4px]  h-4 sm:h-6 xl:h-1",
-          cursorClassName
-        )}></motion.span>
-    </div>)
+        className={cn('block rounded-sm w-[4px]  h-4 sm:h-6 xl:h-1', cursorClassName)}
+      ></motion.span>
+    </div>
   );
 };
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,30 +1,33 @@
-const { heroui } = require("@heroui/react");
+const { heroui } = require('@heroui/react');
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+      fontFamily: {
+        playfair: ['var(--font-playfair-display)'],
+        montserrat: ['var(--font-montserrat)'],
       },
       animation: {
-        "background-position-spin":
-          "background-position-spin 3000ms infinite alternate",
+        'background-position-spin': 'background-position-spin 3000ms infinite alternate',
       },
       keyframes: {
-        "background-position-spin": {
-          "0%": { backgroundPosition: "top center" },
-          "100%": { backgroundPosition: "bottom center" },
+        'background-position-spin': {
+          '0%': { backgroundPosition: 'top center' },
+          '100%': { backgroundPosition: 'bottom center' },
         },
       },
     },
   },
-  darkMode: "class",
+  darkMode: 'class',
   plugins: [heroui()],
 };
